24dcc3c9c9842f4e241549b86f67083f8836389a
Bilal Kazi <bilalahmedkazi@hotmail.com>
2015-12-28 23:25:39 +0000

    Add Navbar and one section
    Very large commit, unusual
    Add a navbar and drawer using JQuery and CSS transitions
    Create a slight navigation system
    Add theme colors
    Change layout slightly

---
+++ src/js/functions.js
@@ -0,0 +1,16 @@
+/*eslint-env jquery */
+
+$(document).ready(function(){
+    $('#navbar-toggle').click(function() {
+        $('#navbar-trigger').toggleClass('show');
+        $('#top').toggleClass('show');
+        $('#mid').toggleClass('show');
+        $('#bot').toggleClass('show');
+        $('#obfus').toggleClass('show');
+        $('#main').toggleClass('scroll');
+    });
+
+    $('h3.toggler').click(function() {
+        $(this).next().slideToggle(400);
+    });
+});
