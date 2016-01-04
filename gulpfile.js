8e8c0cbd8b202f8a679026bd435a6bd28be1c3ca
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-01-04 18:31:54 +0000

    Fix style issues with eslint

--- gulpfile.js
+++ gulpfile.js
@@ -88,56 +88,56 @@ gulp.task('assets', function() {
 // package (see the check-for-favicon-update task below).
 gulp.task('generate-favicon', function(done) {
     realFavicon.generateFavicon({
-    masterPicture: 'img/logo.png',
-    dest: 'favicons/',
-    iconsPath: 'favicons/',
-		design: {
-			ios: {
-				pictureAspect: 'backgroundAndMargin',
-				backgroundColor: '#ffffff',
-				margin: '28%',
-				appName: 'HTML and CSS'
-			},
-			desktopBrowser: {},
-			windows: {
-				pictureAspect: 'whiteSilhouette',
-				backgroundColor: '#603cba',
-				onConflict: 'override',
-				appName: 'HTML and CSS'
-			},
-			androidChrome: {
-				pictureAspect: 'backgroundAndMargin',
-				margin: '17%',
-				backgroundColor: '#ffffff',
-				themeColor: '#9c27b0',
-				manifest: {
-					name: 'HTML and CSS',
-					display: 'browser',
-					orientation: 'notSet',
-					onConflict: 'override'
-				}
-			},
-			safariPinnedTab: {
-				pictureAspect: 'silhouette',
-				themeColor: '#9c27b0'
-			}
-		},
-		settings: {
-			compression: 1,
-			scalingAlgorithm: 'Mitchell',
-			errorOnImageTooSmall: false
-		},
-		markupFile: FAVICON_DATA_FILE
-	}, function() {
-		done();
-	});
+        masterPicture: 'img/logo.png',
+        dest: 'favicons/',
+        iconsPath: 'favicons/',
+        design: {
+            ios: {
+                pictureAspect: 'backgroundAndMargin',
+                backgroundColor: '#ffffff',
+                margin: '28%',
+                appName: 'HTML and CSS'
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'whiteSilhouette',
+                backgroundColor: '#603cba',
+                onConflict: 'override',
+                appName: 'HTML and CSS'
+            },
+            androidChrome: {
+                pictureAspect: 'backgroundAndMargin',
+                margin: '17%',
+                backgroundColor: '#ffffff',
+                themeColor: '#9c27b0',
+                manifest: {
+                    name: 'HTML and CSS',
+                    display: 'browser',
+                    orientation: 'notSet',
+                    onConflict: 'override'
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#9c27b0'
+            }
+        },
+        settings: {
+            compression: 1,
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function() {
+        done();
+    });
 });
 
 // Inject the favicon markups in your HTML pages. You should run
 // this task whenever you modify a page. You can keep this task
 // as is or refactor your existing HTML pipeline.
 gulp.task('inject-favicon-markups', function() {
-	gulp.src([ 'src/index.html' ])
+    gulp.src([ 'src/index.html' ])
 		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
 		.pipe(gulp.dest('./'));
 });
@@ -146,11 +146,11 @@ gulp.task('inject-favicon-markups', function() {
 // released a new Touch icon along with the latest version of iOS).
 // Run this task from time to time. Ideally, make it part of your
 // continuous integration system.
-gulp.task('check-for-favicon-update', function(done) {
-	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
-	realFavicon.checkForUpdates(currentVersion, function(err) {
-		if (err) {
-			throw err;
-		}
-	});
+gulp.task('check-for-favicon-update', function() {
+    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    realFavicon.checkForUpdates(currentVersion, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
 });
