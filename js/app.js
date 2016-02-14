a500c433306097331bc7c1820ae479b71f31e60c
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-02-14 23:36:09 +0000

    trying

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-!function(){var e=angular.module("app",["ngRoute","ngAnimate","hljs"]);e.config(function(e,t){e.when("/",{templateUrl:"views/intro.html"}).when("/htmlInfo",{templateUrl:"views/htmlInfo.html"}).when("/cssInfo",{templateUrl:"views/cssInfo.html"}).when("/tutorial",{templateUrl:"views/tutorial.html"}).when("/resources",{templateUrl:"views/resources.html"}).otherwise({redirectTo:"/"}),t.setOptions({tabReplace:"  "})})}();
+(function() {
+    var app = angular.module('app', ['ngRoute', 'ngAnimate', 'hljs']);
+
+    app.config(function($routeProvider, hljsServiceProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/intro.html'
+            })
+            .when('/htmlInfo', {
+                templateUrl: 'views/htmlInfo.html'
+            })
+            .when('/cssInfo', {
+                templateUrl: 'views/cssInfo.html'
+            })
+            .when('/tutorial', {
+                templateUrl: 'views/tutorial.html'
+            })
+            .when('/resources', {
+                templateUrl: 'views/resources.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        hljsServiceProvider.setOptions({
+            // replace tab with 2 spaces
+            tabReplace: '  '
+        });
+        // $locationProvider.html5Mode(true);
+    });
+}) ();
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,16 @@
-$(document).ready(function(){$("#navbar-toggle").click(function(){$("#navbar-trigger").toggleClass("show"),$("#top").toggleClass("show"),$("#mid").toggleClass("show"),$("#bot").toggleClass("show"),$("#obfus").toggleClass("show"),$("body").toggleClass("scroll")})});
+/*eslint-env jquery */
+
+$(document).ready(function(){
+    $('#navbar-toggle').click(function() {
+        $('#navbar-trigger').toggleClass('show');
+        $('#top').toggleClass('show');
+        $('#mid').toggleClass('show');
+        $('#bot').toggleClass('show');
+        $('#obfus').toggleClass('show');
+        $('body').toggleClass('scroll');
+    });
+
+    // $('h3.toggler').click(function() {
+    //     $(this).next().slideToggle(400);
+    // });
+});
