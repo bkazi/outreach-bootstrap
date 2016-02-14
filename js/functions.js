382ebefde5ab8b9bab4f0ad64e9bc14e8cf3cd9b
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-02-14 23:54:41 +0000

    Fixed issue with links and angular on gh-pages
    Had to change links to 1.5.0 (weird and unexplainable)
    And changing some of the links, get rid of the base thing (dirty links but oh well)

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,1 @@
-/*eslint-env jquery */
-
-$(document).ready(function(){
-    $('#navbar-toggle').click(function() {
-        $('#navbar-trigger').toggleClass('show');
-        $('#top').toggleClass('show');
-        $('#mid').toggleClass('show');
-        $('#bot').toggleClass('show');
-        $('#obfus').toggleClass('show');
-        $('body').toggleClass('scroll');
-    });
-
-    // $('h3.toggler').click(function() {
-    //     $(this).next().slideToggle(400);
-    // });
-});
+$(document).ready(function(){$("#navbar-toggle").click(function(){$("#navbar-trigger").toggleClass("show"),$("#top").toggleClass("show"),$("#mid").toggleClass("show"),$("#bot").toggleClass("show"),$("#obfus").toggleClass("show"),$("body").toggleClass("scroll")})});
