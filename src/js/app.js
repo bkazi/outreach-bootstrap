823feb4ff2c2491ed69f06112669a89fddef64cf
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-02-14 22:54:59 +0000

    Change some links now
    That did work yay, hopefully this does too

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,23 @@
 
     app.config(function($routeProvider, hljsServiceProvider, $locationProvider) {
         $routeProvider
-            .when('/', {
+            .when('', {
                 templateUrl: 'views/intro.html'
             })
-            .when('/htmlInfo', {
+            .when('htmlInfo', {
                 templateUrl: 'views/htmlInfo.html'
             })
-            .when('/cssInfo', {
+            .when('cssInfo', {
                 templateUrl: 'views/cssInfo.html'
             })
-            .when('/tutorial', {
+            .when('tutorial', {
                 templateUrl: 'views/tutorial.html'
             })
-            .when('/resources', {
+            .when('resources', {
                 templateUrl: 'views/resources.html'
             })
             .otherwise({
-                redirectTo: '/'
+                redirectTo: ''
             });
         hljsServiceProvider.setOptions({
             // replace tab with 2 spaces
